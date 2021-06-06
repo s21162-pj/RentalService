75ec2055a3bdfa43289fd790a33b349f04e97868
s21162-pj <s21162@pjwstk.edu.pl>
2021-06-06 18:40:19 +0200

    asd

--- src/main/java/pl/pjatk/robbrz/Controller/RentalController.java
+++ src/main/java/pl/pjatk/robbrz/Controller/RentalController.java
@@ -1,6 +1,10 @@
 package pl.pjatk.robbrz.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pl.pjatk.robbrz.Service.RentalService;
+import pl.pjatk.robbrz.model.Movie;
 
 public class RentalController {
     private final RentalService rentalService;
@@ -8,4 +12,9 @@ public class RentalController {
     public RentalController(RentalService rentalService) {
         this.rentalService = rentalService;
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
+        return ResponseEntity.ok(rentalService.getMovie(id));
+    }
+
 }
--- src/main/java/pl/pjatk/robbrz/Service/RentalService.java
+++ src/main/java/pl/pjatk/robbrz/Service/RentalService.java
@@ -1,11 +1,17 @@
 package pl.pjatk.robbrz.Service;
 
 import org.springframework.web.client.RestTemplate;
+import pl.pjatk.robbrz.model.Movie;
 
 public class RentalService {
     private final RestTemplate restTemplate;
 
     public RentalService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public Movie getMovie(Long id) {
+        Movie movie = restTemplate.getForEntity("http://localhost:8080/movies/" + id, Movie.class).getBody();
+        return movie;
     }
 }
